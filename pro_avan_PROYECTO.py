574e9570b85061fd7468102199e3fc7712e94d66
RayssaHA17 <rayssa.hidalgo@upch.pe>
2025-06-27 18:11:31 -0500

    Update pro_avan_PROYECTO.py

--- pro_avan_PROYECTO.py
+++ pro_avan_PROYECTO.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import folium
+import plotly.express as px
 from folium.plugins import HeatMap
 from streamlit_folium import st_folium
 
